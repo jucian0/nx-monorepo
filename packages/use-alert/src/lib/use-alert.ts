4d924582237b3d7049afaf1ae16e82a61acb454c
Jucian0 <juciano@outlook.com.br>
2022-06-26 02:23:46 -0300

    feat: add commitlint e husky

--- packages/use-alert/src/lib/use-alert.ts
+++ packages/use-alert/src/lib/use-alert.ts
@@ -1,3 +1,6 @@
 export function useAlert(): string {
   return 'use-alert2222';
 }
+// npx nx affected --target=publish --base=remotes/origin/main~1 --parallel=1
+
+// in order to avoid redeploy, try to compare, package.json differences, to publish just when version is bumped!

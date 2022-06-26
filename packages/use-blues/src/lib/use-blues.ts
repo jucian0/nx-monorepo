2249c012ab4a8de9fd68cf6484d532663d4f6e84
Jucian0 <juciano@outlook.com.br>
2022-06-26 01:54:05 -0300

    feat: add commitlint e husky

--- packages/use-blues/src/lib/use-blues.ts
+++ packages/use-blues/src/lib/use-blues.ts
@@ -1,12 +1,3 @@
-import { useElement } from '@nx-test/use-elememnt';
-import { useFala } from '@nx-test/use-fala';
-import { useRoot } from '@nx-test/use-root';
-import { useTest } from '@nx-test/use-test';
-
 export function useBlues(): string {
-  useElement();
-  useFala();
-  useRoot();
-  useTest();
   return 'use-blues 2';
 }
--- packages/use-element/src/lib/use-element.ts
+++ packages/use-element/src/lib/use-element.ts
@@ -1,6 +1,3 @@
-import { useTest } from '@nx-test/use-test';
-
 export function useElement(): string {
-  useTest();
   return 'use-element 2';
 }

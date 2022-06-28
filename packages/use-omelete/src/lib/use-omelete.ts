1e6c54b5d773559ed39c82888ad700b99eea45fa
Jucian0 <juciano@outlook.com.br>
2022-06-28 12:49:39 -0300

    fix: fixing ci

--- packages/use-omelete/src/lib/use-omelete.ts
+++ packages/use-omelete/src/lib/use-omelete.ts
@@ -1,3 +1,6 @@
+import { useRoot } from '@nx-test/use-root';
+
 export function useOmelete(): string {
+  useRoot();
   return 'use-omelete22';
 }
--- packages/use-root/src/lib/use-root.ts
+++ packages/use-root/src/lib/use-root.ts
@@ -1,3 +1,3 @@
 export function useRoot(): string {
-  return 'use-root 9';
+  return 'use-root 10';
 }

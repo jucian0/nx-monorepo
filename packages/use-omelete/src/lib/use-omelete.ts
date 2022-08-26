4996839222d8d688c572108bbe85e60614d23ec1
jucian0 <juciano@outlook.com.br>
2022-08-25 23:28:31 -0300

    feat(playground) - add codeblock styles

--- packages/use-omelete/src/lib/use-omelete.ts
+++ packages/use-omelete/src/lib/use-omelete.ts
@@ -4,6 +4,5 @@ import {root} from 'root'
 export function useOmelete(): string {
   root()
   useRoot();
-  console.log('alo')
   return 'use-omelete22';
 }
--- packages/use-root/src/index.ts
+++ packages/use-root/src/index.ts
@@ -1,2 +1,3 @@
 export * from './lib/use-root';
 export const test = ''
+console.log('alo')
